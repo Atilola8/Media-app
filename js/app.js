0c325e414e9e77ef9578a90e23bd4102202247ce
Atilola8 <70245894+Atilola8@users.noreply.github.com>
2020-09-04 22:07:56 -0400

    added more code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 function setup() {
-  createCanvas(800, 600);
+  var red = "#ff0000";
+  let thickness = 100;
 
-  rect(30, 20, 55, 55);
-  rect(100, 20, 55, 55);
+  createCanvas(800, 600);
+  fill("yellow");
+
+  stroke(20);
+  ellipse(76, 46, 55, 55);
+  fill("red");
+
+  circle(199, 100, 50);
+  fill("blue");
+
+  rect(120, 150, 120, 45);
+  fill("gray");
+
+  rect(200, 220, 150, 50);
+  fill("blue");
+
+  circle(320, 170, 50);
+  stroke(20);
+  line(10, 7, 120, 150);
 }
