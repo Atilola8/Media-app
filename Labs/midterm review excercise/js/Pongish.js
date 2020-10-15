171ba95c197fec2411df16a49ce40e9e30e71809
Atilola8 <70245894+Atilola8@users.noreply.github.com>
2020-10-15 16:30:03 -0400

    Adding pongish

---
+++ Labs/midterm review excercise/js/Pongish.js
@@ -0,0 +1,50 @@
+// constantly changing the x- and y-coordinates of the ball and  to animate it, so it would be better to store those in variables. These var are also meant to keep track on the codes
+var xBallChange = 5;
+var yBallChange = 5;
+var xBall = Math.floor(Math.random() * 300) + 50;
+var yBall = 50;
+var diameter = 50;
+var xPaddle = 380;
+var yPaddle;
+var paddleWidth = 100;
+var paddleHeight = 25;
+var started = false;
+//creates the background canvas we are working on and it can only work once.
+function setup() {
+  createCanvas(800, 600);
+}
+//where the whole codes runs and is allowed to keep running.
+function draw() {
+  background(175, 175, 255);
+  //adding color to the ball
+  fill("red");
+
+  noStroke();
+  //introducing the ball
+  ellipse(xBall, yBall, 50, 50);
+  //to move the ball
+  xBall += xBallChange;
+  yBall += yBallChange;
+  //This code below checks to see if the ball’s x-coordinate is between 0 and windowWidth, and if the ball’s y-coordinate is between 0 and windowHeight. P5 javascript
+  if (xBall < 50 / 2 || xBall > 800 - 0.5 * 50) {
+    xBallChange *= -1;
+  }
+  if (yBall < 50 / 2 || yBall > 600 - 50) {
+    yBallChange *= -1;
+  }
+  if (!started) {
+    xPaddle = 800 / 2;
+    yPaddle = 600 - 300;
+    started = false;
+  }
+  fill(0, 255, 255);
+  noStroke();
+  rect(760, yPaddle, 20, 100);
+  //making the paddle move up and down
+  if (keyIsDown(UP_ARROW)) {
+    xPaddle -= 50;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    xPaddle -= 50;
+  }
+}
