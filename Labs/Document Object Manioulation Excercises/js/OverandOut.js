676cacb658fad31fdc95de36a8a09c197a56549b
Atilola8 <70245894+Atilola8@users.noreply.github.com>
2020-11-04 16:55:17 -0500

    add document

---
+++ Labs/Document Object Manioulation Excercises/js/OverandOut.js
@@ -0,0 +1,8 @@
+//To change the background color of the square when hover on it 
+function myOverFunction(){
+    document.getElementsByClassName("square")[0].style.backgroundColor = "black"
+}
+//To return the background color to the intial background color which is blue when the mouse is removed 
+function myOutFunction(){
+    document.getElementsByClassName("square")[0].style.backgroundColor = "Blue"
+}
